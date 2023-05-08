e82f305402958e834cb3dfa6a8f88c29122aa45a
Sebastien Mondet <seb@mondet.org>
2023-05-08 17:06:04 -0400

    Implement some of the actions and get-statuses

--- bin/main.ml
+++ bin/main.ml
@@ -1,9 +1,15 @@
 let dbgf fmt = Format.kasprintf (Printf.eprintf "%s\n%!") fmt
+let str = Format.asprintf
 
 open! Base
 
 module Action = struct
   type t = Command of string [@@deriving sexp, variants, equal, compare]
+end
+
+module UI = struct
+  type t = Web_localhost of { port : int }
+  [@@deriving sexp, variants, equal, compare]
 end
 
 module Service = struct
@@ -12,12 +18,14 @@ module Service = struct
     start : Action.t;
     stop : Action.t;
     get_status : Action.t;
+    ui : UI.t option;
   }
   [@@deriving sexp, fields, equal, compare, make]
 
   module Example = struct
     let ml_donkey_docker =
       make "MLDonkey"
+        ~ui:(UI.web_localhost ~port:4080)
         ~start:
           (Action.command
              "docker run --rm -d --name mldonkey-service -v \
@@ -39,6 +47,7 @@ end
 module Configuration = struct
   type t = {
     default_browser_command : string option;
+    debug_mode : bool; [@default true]
     services : Service.t list; [@main]
   }
   [@@deriving sexp, fields, equal, compare, make]
@@ -55,14 +64,45 @@ module App_icon = struct
 end
 
 module State = struct
+  module Service_data = struct
+    type t = {
+      service : Service.t; [@main]
+      mutable last_status : (unit, string) Result.t;
+      mutable status_thread : Thread.t option;
+    }
+    [@@deriving fields, make]
+
+    let pp fmt { service; last_status; status_thread } =
+      let open Caml.Format in
+      fprintf fmt "(%s %s -- thread: %s)"
+        (Service.display_name service)
+        (match last_status with Ok () -> "OK" | Error s -> str "Dead: %s" s)
+        (Option.value_map
+           ~f:(fun s -> Thread.id s |> Int.to_string_hum)
+           ~default:"NONE" status_thread)
+  end
+
   type t = {
     mutable configuration : Configuration.t option;
     mutable configuration_path : string option;
     mutable main_windows : GWindow.window list;
+    mutable debug_messages : string list;
+    mutable services_state : Service_data.t list;
   }
   [@@deriving fields, make]
 
   let create () = make ()
+
+  let handle_configuration_change state =
+    match configuration state with
+    | Some conf ->
+        state.services_state <-
+          List.map
+            ~f:
+              (Service_data.make ~last_status:(Error "Not initialized")
+                 ?status_thread:None)
+            (Configuration.services conf)
+    | None -> state.services_state <- []
 
   let load_configuration_path state path =
     let b = Buffer.create 42 in
@@ -74,11 +114,29 @@ module State = struct
     in
     state.configuration <- Some configuration;
     state.configuration_path <- Some path;
+    handle_configuration_change state;
     ()
 
   let load_example state configuration =
     state.configuration <- Some configuration;
+    handle_configuration_change state;
     ()
+
+  let if_debug state f =
+    match configuration state with
+    | None | Some { Configuration.debug_mode = false; _ } -> ()
+    | _ -> f ()
+
+  let add_debug_message state msg =
+    if_debug state (fun () ->
+        dbgf "Debug message: %s" msg;
+        state.debug_messages <- msg :: state.debug_messages)
+
+  let browser_command state =
+    let actual_default = "chromium-browser --user-data-dir=/tmp/ost-bro/" in
+    match configuration state with
+    | Some { default_browser_command = Some c; _ } -> c
+    | _ -> actual_default
 end
 
 let protect_exn f =
@@ -96,6 +154,50 @@ let not_implemented s () =
   Caml.Printf.eprintf "%s\n%!" msg;
   GToolbox.message_box ~title:"Error: Not Implemented" msg;
   ()
+
+module Run = struct
+  let command_succeeds s =
+    match Caml.Sys.command s with
+    | 0 -> ()
+    | other -> failwith (str "Command %S returned: %d" s other)
+
+  let action ?(protect = protect_exn) state = function
+    | Action.Command s ->
+        State.add_debug_message state (str "Running command: %s" s);
+        protect (fun () -> command_succeeds s)
+
+  let start_service state service () = action state (Service.start service)
+  let stop_service state service () = action state (Service.stop service)
+
+  let visit_service state service () =
+    match Service.ui service with
+    | None ->
+        failwith (str "Service %S has no UI" (Service.display_name service))
+    | Some (UI.Web_localhost { port }) ->
+        command_succeeds
+          (str "%s http://localhost:%d" (State.browser_command state) port)
+
+  let start_status_thread state service =
+    let rec update_status () =
+      dbgf "Service_data: %a" State.Service_data.pp service;
+      begin
+        try
+          action
+            ~protect:(fun f -> f ())
+            state
+            (Service.get_status (State.Service_data.service service));
+          State.Service_data.set_last_status service (Ok ())
+        with e ->
+          State.Service_data.set_last_status service
+            (Error (str "Exception: %a" Exn.pp e))
+      end;
+      Thread.delay 5.;
+      update_status ()
+    in
+    let thd = Thread.create update_status () in
+    State.Service_data.set_status_thread service (Some thd);
+    ()
+end
 
 let show_window state =
   let window =
@@ -119,6 +221,7 @@ let start_application load_state =
   let _ = GMain.init () in
   let state = State.create () in
   load_state state;
+  List.iter (State.services_state state) ~f:(Run.start_status_thread state);
   let tray_icon =
     (* GMisc.status_icon_from_icon_name "utilities-terminal" *)
     GMisc.status_icon_from_pixbuf (App_icon.as_pixbuf ())
@@ -138,10 +241,10 @@ let start_application load_state =
                         `M
                           ( Service.display_name service,
                             [
-                              `I ("Start", not_implemented "start");
+                              `I ("Start", Run.start_service state service);
                               `I ("Get Status", not_implemented "status");
-                              `I ("Stop", not_implemented "stop");
-                              `I ("Show UI", not_implemented "show ui");
+                              `I ("Stop", Run.stop_service state service);
+                              `I ("Show UI", Run.visit_service state service);
                             ] )) )
           end
       in
